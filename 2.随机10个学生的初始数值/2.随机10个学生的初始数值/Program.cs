2766a973778b534b4d318fdaf2018ba3db8462c5
vanillamia <37062289+vanillamia@users.noreply.github.com>
2018-07-20 15:41:46 +0800

    追加了学生姓名等
    姓名可以从一个List里抽取了
    追加了“性别”“性格”“性取向”三个标签

--- 2.随机10个学生的初始数值/2.随机10个学生的初始数值/Program.cs
+++ 2.随机10个学生的初始数值/2.随机10个学生的初始数值/Program.cs
@@ -18,9 +18,29 @@ namespace 随机10个学生的初始数值
         Random p_r = new Random();
         //每个等级的数值也需要随机，再来个种子
         Random p_status = new Random();
+        //学生外向还是内向，男性还是女性
+        Random p_sex = new Random();
+        Random p_per = new Random();
+        //学生是同性恋还是异性恋
+        Random p_ori = new Random();
+        //随机抽取一个名字
+        
+
+
+        private string GetName()
+        {
+            //从列表中取得一个名字
+            if (p_sex.NextDouble() < 0.5)
+                return "男生";
+            else
+                return "女生";
+        }
+
+        //【方法】从List中拿出一个名字，然后删掉该名字
+       
 
         //【方法】//随机获得一个学生的等级
-        public int GeneStuLevel()
+        private int GeneLev()
         {
             //生成一个1到12的随机数,然后乘以概率
             double i = p * (p_r.Next(1, 13));
@@ -36,13 +56,47 @@ namespace 随机10个学生的初始数值
                 return 1;
         }
 
-        //【方法】//获得每个学生的等级和四项属性
-        public void GeneStuStatus(out int stuLevel, out int stu, out int spo, out int reser, out int cha)
+        private string GeneSex()
         {
-            stuLevel = this.GeneStuLevel(); //随机生成介个学生的等级
+            //50%几率男，50%几率女
+            if (p_sex.NextDouble() < 0.5)
+                return "男生";
+            else
+                return "女生";
+        }
+
+        private string GenePer()
+        {
+            //50%几率外向，50%几率内向
+            if (p_per.NextDouble() < 0.5)
+                return "外向";
+            else
+                return "内向";
+        }
+
+        private string GeneOri()
+        {
+            //80%几率异性恋，20%几率同性恋
+            int pro_sexOrient = 80;
+            if (p_sex.Next(100) < pro_sexOrient)
+                return "异性恋";
+            else
+                return "同性恋";
+        }
+
+
+        //【方法】//获得每个学生的等级和四项属性, 
+        public void GeneStuStatus(out int stuLev,out string stuPer, out string stuSex, out string stuSexOri, out int stu, out int spo, out int reser, out int cha)
+        {
+            
+            stuLev = this.GeneLev(); //随机生成介个学生的等级
+            stuPer = this.GenePer();//随机生成介个学生的个性
+            stuSex = this.GeneSex(); //随机生成介个学生的性别
+            stuSexOri = this.GeneOri(); //随机生成介个学生的性取向
+
             int[] levelStatus = new int[5] { 1, 51, 81, 121, 151 }; //5个等级的初始值，以后说不定会改啦(・//ω//・)
 
-            switch (stuLevel)
+            switch (stuLev)
             {
                 case 1:
                     stu = levelStatus[0] + (int)(50 * (p_status.NextDouble()));
@@ -97,14 +151,28 @@ namespace 随机10个学生的初始数值
                 int sports = 0;
                 int research = 0;
                 int charm = 0;
+                string stuName, stuSex, stuPersonality, stuSexOrient; 
                 //初始化学生的等级
                 int stuLevel = 1;
+                List<string> allNames = new List<string>();
+                string[] names = new string[] { "玩偶", "百合子", "王安泽", "GAL", "lika", "章鱼", "mili", "零", "SB老师", "水管", "世界线","inovker","代码星人" };
+                allNames.AddRange(names);
+                
+            
                 StudentStatus test = new StudentStatus();
+                List<string> nameList = new List<string>();
+                Random r_name = new Random();
+                for (int i=0; i < 10; i++) {
+                    int m = (int)(r_name.Next(allNames.Count()));
+                    nameList.Add(allNames[m]);
+                    allNames.RemoveAt(m);
+                 }
 
                 for (int i = 1; i <= 10; i++)
                 {
-                    test.GeneStuStatus(out stuLevel, out study, out sports, out research, out charm);
-                    Console.WriteLine("{0}号学生，等级：【{1}】，学习：{2}，运动：{3}，研究：{4}，魅力：{5}", i, stuLevel, study, sports, research, charm);
+                    test.GeneStuStatus(out stuLevel, out stuPersonality, out stuSex, out stuSexOrient, out study, out sports, out research, out charm);
+                    stuName = nameList[i - 1];
+                    Console.WriteLine("{0, 2}号学生 {1,-6} 等级{2} 学习{3,-3} 运动{4,-3} 研究{5,-3} 魅力{6,-3} {7} {8} {9}", i, stuName, stuLevel, study, sports, research, charm, stuSex, stuPersonality, stuSexOrient);
                 }
                 
                 Console.ReadLine();
